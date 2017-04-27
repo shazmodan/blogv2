d252864424e65966284a53f96a1b45a6417549d4
William Bergqvist <william.bergqvist@if.se>
2017-04-27 12:14:17 +0200

    Updated rollup to run a dev config without minification for example

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -4,11 +4,11 @@ import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-js-harmony';
+//import uglify from 'rollup-plugin-uglify';
+//import { minify } from 'uglify-js-harmony';
 import postcss from 'rollup-plugin-postcss';
 import cssnext from 'postcss-cssnext';
-import cssnano from 'cssnano';
+//import cssnano from 'cssnano';
 
 export default {
   entry: 'src/client/main.js',
@@ -18,7 +18,7 @@ export default {
     postcss({
       plugins: [
         cssnext({ warnForDuplicates: false }),
-        cssnano()
+        //cssnano()
       ],
       extensions: ['.css']
     }),
@@ -34,7 +34,7 @@ export default {
         // 'node_modules/fetch/lib/fetch.js': [ 'fetchUrl' ]
       }
     }),
-    buble(),
-    uglify({}, minify),
+    buble()//,
+    //uglify({}, minify),
   ]
 }
